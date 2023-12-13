56f860f1a41179fa2120260b951a019a460cdb9a
Mohammad Nourizadeh <mo.nourizadeh1@gmail.com>
2023-12-13 17:30:44 +0330

    clear btn added

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,16 +4,26 @@ const tagColor = document.getElementById('tagColor');
 const tagWidth = document.getElementById('tagWidth');
 const tagHeight = document.getElementById('tagHeight');
 const createdElementCol = document.getElementById('created-element-col');
+const clearBtn = document.getElementById('clear-btn');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    clearBtn.classList.remove('d-none');
     const element = document.createElement(tagName.value);
     element.style.backgroundColor = tagColor.value;
     element.style.width = `${tagWidth.value}px`;
     element.style.height = `${tagHeight.value}px`;
     element.innerText = tagName.value;
-    element.classList.add('rounded-5', 'd-flex', 'justify-content-center', 'align-items-center', 'shadow' , 'mb-2')
+    element.classList.add('rounded-5', 'd-flex', 'justify-content-center', 'align-items-center', 'shadow' , 'mb-2');
 
-    createdElementCol.appendChild(element)
+    createdElementCol.appendChild(element);
+})
+
+
+clearBtn.addEventListener('click' , (e) => {
+    e.preventDefault();
+
+    createdElementCol.innerHTML = '';
+    clearBtn.classList.add('d-none');
 })
